da27e5ecfccd9e2d5555a9f942f3e3efc5134d74
EC2 Default User <ec2-user@ip-172-31-29-113.ec2.internal>
2026-04-06 19:38:34 +0000

    sg

--- securitygroup/main.tf
+++ securitygroup/main.tf
@@ -1,54 +1,54 @@
 provider "aws" {
-region = "us-east-1"
-alias = "virginia"
+  region = "us-east-1"
+  alias  = "virginia"
 }
 
 provider "aws" {
-region = "us-west-2"
-alias = "oregon"
+  region = "us-west-2"
+  alias  = "oregon"
 }
 
 resource "aws_instance" "swiggy" {
-tags = {
-Name = "delivery"
-}
-provider = aws.virginia
-ami = "ami-01b14b7ad41e17ba4"
-instance_type = var.type["swigg"]
-key_name = var.key
-availability_zone = var.zone["swiggy"]
-vpc_security_group_ids = aws_security_group.mysg.id
+  tags = {
+    Name = "delivery"
+  }
+  provider               = aws.virginia
+  ami                    = "ami-01b14b7ad41e17ba4"
+  instance_type          = var.type["swigg"]
+  key_name               = var.key
+  availability_zone      = var.zone["swiggy"]
+  vpc_security_group_ids = aws_security_group.mysg.id
 }
 
 resource "aws_instance" "zomato" {
-tags = {
-Name = "delivery"
-}
-provider = aws.oregon
-ami = "ami-043ab4148b7bb33e9"
-instance_type = var.type["zomato"]
-key_name = "de"
-availability_zone = var.zone["zomato"]
-root_block_device {
-volume_size = 10
-}
+  tags = {
+    Name = "delivery"
+  }
+  provider          = aws.oregon
+  ami               = "ami-043ab4148b7bb33e9"
+  instance_type     = var.type["zomato"]
+  key_name          = "de"
+  availability_zone = var.zone["zomato"]
+  root_block_device {
+    volume_size = 10
+  }
 }
 
 variable "type" {
-type = map(string)
-default = {
-swigg = "t2.micro"
-zomato = "t3.micro"
-}
+  type = map(string)
+  default = {
+    swigg  = "t2.micro"
+    zomato = "t3.micro"
+  }
 }
 variable "key" {
-type = string
-default = "dem"
+  type    = string
+  default = "dem"
 }
 variable "zone" {
-type = map(string)
-default = {
-swiggy = "us-east-1a"
-zomato = "us-west-2b"
-}
+  type = map(string)
+  default = {
+    swiggy = "us-east-1a"
+    zomato = "us-west-2b"
+  }
 }
--- securitygroup/sg.tf
+++ securitygroup/sg.tf
@@ -1,22 +1,22 @@
 resource "aws_security_group" "mysg" {
-name = "terraform-sg"
-description = "for my swiggy security group"
-ingress {
-from_port = 22
-to_port = 22
-protocol = "tcp"
-cidr_blocks = ["0.0.0.0/0"]
-}
-ingress {
-from_port = 8080
-to_port = 8081
-protocol = "tcp"
-cidr_blocks = ["0.0.0.0/0"]
-}
-egress {
-from_port = 0
-to_port = 0
-protocol = "-1"
-cidr_blocks = ["0.0.0.0/0"]
-}
+  name        = "terraform-sg"
+  description = "for my swiggy security group"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
