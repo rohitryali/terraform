2ee980b184eb94bba097a30c1e1af1d3ac4229b4
rohitryali <rohitryali9@gmail.com>
2026-04-18 00:03:18 +0530

    Update elb.tf

--- asg/elb.tf
+++ asg/elb.tf
@@ -1,14 +1,35 @@
-resource "aws_elb" "myelb" {
-name = "myelb"
-subnets = [aws_subnet.subnet-1.id, aws_subnet.subnet-2.id]
-security_groups = [aws_security_group.mysg.id]
-listener {
-instance_port = 80
-instance_protocol = "http"
-lb_port = 80
-lb_protocol = "http"
+resource "aws_lb" "myalb" {
+  name               = "myalb"
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.subnet-1.id, aws_subnet.subnet-2.id]
+  security_groups    = [aws_security_group.mysg.id]
+
+  tags = {
+    Name = "myalb"
+  }
 }
-tags = {
-Name = "myelb"
+resource "aws_lb_target_group" "mytg" {
+  name     = "mytg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.myvpc.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
 }
+resource "aws_lb_listener" "mylistener" {
+  load_balancer_arn = aws_lb.myalb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.mytg.arn
+  }
 }
