a3eecba0ed6469ca449e83e9342ef28d2e9acbfe
rohitryali <rohitryali9@gmail.com>
2026-04-17 23:58:42 +0530

    Update asg.tf

--- asg/asg.tf
+++ asg/asg.tf
@@ -2,6 +2,7 @@ resource "aws_autoscaling_group" "myasg" {
 name = "myasg"
 launch_template {
 id = aws_launch_template.my_template.id
+version = "$Latest"
 }
 vpc_zone_identifier = [aws_subnet.subnet-1.id, aws_subnet.subnet-2.id]
 load_balancers = [aws_elb.myelb.name]
