8e096866ae3e5bee79fb19aa2821ce06f6b49797
rohitryali <rohitryali9@gmail.com>
2026-04-18 00:13:49 +0530

    Update asg.tf

--- asg/asg.tf
+++ asg/asg.tf
@@ -5,7 +5,7 @@ id = aws_launch_template.my_template.id
 version = "$Latest"
 }
 vpc_zone_identifier = [aws_subnet.subnet-1.id, aws_subnet.subnet-2.id]
-load_balancers = [aws_elb.myelb.name]
+target_group_arns = [aws_lb_target_group.mytg.arn]
 desired_capacity = 3
 min_size = 2
 max_size =5
